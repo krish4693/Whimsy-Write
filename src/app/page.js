75fb9dace1af8fe7419aae627f8353749eaeefd3
Krishnanand U <krishnanand.u@armiasystems.com>
2024-08-05 18:14:00 +0530

    chore: removed unwanted styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 export default function Home() {
   return (
     <>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum optio suscipit explicabo quasi delectus id sint excepturi eius quos aperiam. Voluptatum, sunt. Totam eum debitis rerum officia sequi, voluptatem error!</h1>
-        <Link href="/">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur delectus voluptates pariatur sint laboriosam alias quas officiis cumque quae, totam, sunt rem nam dolore recusandae deserunt exercitationem qui iste eum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium vel explicabo sit vero placeat sint quidem dolor dolorem ratione magni illum earum tempore, repellendus dignissimos autem eligendi, nulla voluptatem <enim className="lorem">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem nesciunt quisquam, numquam odit dicta perspiciatis inventore obcaecati esse, nemo doloremque quas placeat fugit, perferendis fugiat officiis dolor deserunt itaque error?</enim>
-        </Link>
+        <Navbar/>
+        <Footer/>
     </>
   );
 }
