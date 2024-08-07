1678aa2dc801f00238dd14142e8b0a6d87db6730
Krishnanand U <krishnanand.u@armiasystems.com>
2024-08-07 17:23:18 +0530

    refactor: added images to public folder

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import styles from './categoryList.module.css'
 
+const linklist=[{
+  title:"style",
+  imageLink:"./style.png"
+},
+{
+  title:"fashion",
+  imageLink:"./fashion.png"
+},
+
+]
+
 const Featured = () => {
   return (
-    <div className={styles.container}>categoryList</div>
+    <div className={styles.container}>
+      <h1>Popular Categories</h1>
+      <div className={styles.categories}>
+
+      </div>
+    </div>
   )
 }
 
