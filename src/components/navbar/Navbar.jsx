2c08ebe3bd3b78e42b74e53940f94ca4bfa9c9bc
Krishnanand U <krishnanand.u@armiasystems.com>
2024-08-07 12:43:12 +0530

    chore : responsive navbar completed, navbar authlinks seek status to see if it's authenticated or not. responsive navigation is set up, triggered by hamburger button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,12 +14,12 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>Whimsy-Write</div>
+      <div className={styles.logo}>whimsyWrite</div>
       <div className={styles.links}>
         <ThemeToggle/>
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link className={styles.link} href="/">Homepage</Link>
+        <Link className={styles.link} href="/">Contact</Link>
+        <Link className={styles.link} href="/">About</Link>
         <AuthLinks/>
       </div>
     </div>
