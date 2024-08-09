7787e63284fa0c5860a0caecfbf270ae3b05ba10
Krishnanand U <krishnanand.u@armiasystems.com>
2024-08-09 13:03:58 +0530

    chore: added style for menu page

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -10,13 +10,22 @@ const Menu = () => {
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <MenuPosts withImage={false} />
-      <h2 className={styles.subtitle}>Discover by topic</h2>
-      <h1 className={styles.title}>Categories</h1>
-      <MenuCategories />
-      <h2 className={styles.subtitle}>Chosen by the editor</h2>
-      <h1 className={styles.title}>Editors Pick</h1>
-      <MenuPosts withImage={true} />
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image}></Image>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}` }>Travel</span>
+            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet consecteur adipisicing</h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>John Doe</span>
+            <span className={styles.date}>24.03.24</span>
+          </div>
+          </div>
+        </Link>
+      </div>
+
     </div>
   );
 };
