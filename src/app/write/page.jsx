35186fa61ad3228afc23d965756bdee7f3ad1d25
Krishnanand U <krishnanand.u@armiasystems.com>
2024-08-15 15:50:10 +0530

    chore: completed write blog design

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,25 +1,50 @@
-"use client"
+"use client";
+import Image from "next/image";
+import styles from "./writePage.module.css";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
-import Image from "next/image"
-import styles from './writePage.module.css'
-import {useState} from 'react'
-import ReactQuill from "react-quill"
-import "react-quill/dist/quill.bubble.css"
+const WritePage = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
 
-
-
-
-const writePage = () => {
   return (
     <div className={styles.container}>
-        <input type="text" placeholder="Title" className={styles.input} />
-        <div className={styles.editor}>
-            <button className={styles.button}>
-                
+      <input type="text" placeholder="Title" className={styles.input}/>
+      <div className={styles.editor}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
+          <Image src="/plus.png" alt="Add" width={16} height={16} />
+        </button>
+        {open && (
+          <div className={styles.add}>
+            <button className={styles.addButton}>
+              <Image src="/image.png" alt="Image" width={16} height={16} />
             </button>
-        </div>
+            <button className={styles.addButton}>
+              <Image
+                src="/external.png"
+                alt="External"
+                width={16}
+                height={16}
+              />
+            </button>
+            <button className={styles.addButton}>
+              <Image src="/video.png" alt="Video" width={16} height={16} />
+            </button>
+          </div>
+        )}
+        <ReactQuill
+          className={styles.textArea}
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+        />
+      </div>
+      <button className={styles.publish}>Publish</button>
     </div>
-  )
-}
+  );
+};
 
-export default writePage
+export default WritePage;
