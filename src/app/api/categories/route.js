89b4adc576186e4fc4c5101b35ca6543393e5def
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-08-21 15:42:43 +0530

    chore: created category routes

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server"
+
+export const GET=async()=>{
+    try {
+        const categories= await prisma.categories.findMany()
+        return new NextResponse(JSON.stringify(categories,{status:200}))
+        
+    } catch (err) {
+        console.log(err)
+        return new NextResponse(JSON.stringify({message:"Something went wrong"},{status:500}))
+        
+    }
+}
