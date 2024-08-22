a00478381e9ccb071c33ec29896f88e3671637ef
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-08-22 15:16:19 +0530

    chore: pagination api fixed

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,14 +5,14 @@ export const GET=async(req)=>{
 
     const {searchParams}=new URL(req.url)
     const page=searchParams.get("page")
-    const POST_PER_PAGE=2
+    const POST_PER_PAGE=4
 
     try {
         const posts= await prisma.Post.findMany(
-            // {
-            //     take:POST_PER_PAGE,
-            //     skip:POST_PER_PAGE*(page-1)
-            // }
+            {
+                take:POST_PER_PAGE,
+                skip:POST_PER_PAGE*(page)
+            }
         )
         return new NextResponse(JSON.stringify(posts,{status:200}))
         
