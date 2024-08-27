eb2b81787bd9ad46bba1b3ee7e82c691843179cd
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-08-27 17:47:42 +0530

    feat: pressing on posts redirects to post page with specific post

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import styles from "./card.module.css";
 import Image from "next/image";
+import Link from "next/link";
+
 
 const Card = ({ item }) => {
   const imageUrl = item.img || "https://via.placeholder.com/400x300?text=No+Image+Available";
 
   return (
     <div className={styles.card}>
-      <Image
-        src={imageUrl}
-        alt={item.title}
-        width={400}
-        height={300}
-        className={styles.cardImage}
-      />
-      <h2 className={styles.cardTitle}>{item.title}</h2>
-      <p className={styles.cardDescription}>{item.desc}</p>
+      <Link href={`/posts/${item.slug}`}>
+        <Image
+          src={imageUrl}
+          alt={item.title}
+          width={400}
+          height={300}
+          className={styles.cardImage}
+        />
+        <h2 className={styles.cardTitle}>{item.title}</h2>
+        <p className={styles.cardDescription}>{item.desc}</p>
+      </Link>
+
     </div>
   );
 };
