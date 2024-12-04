ff79704c9a4f12d0b4e9ad6fda0ce92d3fc2f3f6
krish4693 <krishnanand.u@armiasystems.com>
2024-12-04 17:35:00 +0530

    fix: change format of date in publish

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,32 @@ import Image from "next/image";
 import Link from "next/link";
 
 
+
 const Card = ({ item }) => {
   const imageUrl = item.img || "https://via.placeholder.com/400x300?text=No+Image+Available";
+
+
+  function formatDate(isoString) {
+    const date = new Date(isoString);
+
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const month=date.getMonth()
+
+    // const months = [
+    //   "January", "February", "March", "April", "May", "June",
+    //   "July", "August", "September", "October", "November", "December"
+    // ];
+
+
+    // Return the formatted string
+    return `${day}.${month}.${year}`;
+  }
+
+  // Example usage
+  const isoDate = "2024-12-04T10:45:53.254Z";
+  console.log(formatDate(isoDate)); // Output: 4-December-2024
+
 
   return (
     <div className={styles.card}>
@@ -21,7 +45,8 @@ const Card = ({ item }) => {
         />
         <div>
           <h2 className={styles.cardTitle}>{item.title}</h2>
-          <div className={styles.cardDescription} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0,60) }}/>
+          <div className={styles.cardDescription} dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }} />
+          <div>Published on {formatDate(item?.createdAt)}</div>
         </div>
       </Link>
 
