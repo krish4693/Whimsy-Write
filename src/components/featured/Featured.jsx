a1e3ebb6256ab4dbbfc1e9901d4182028064da72
krish4693 <krishnanand.u@armiasystems.com>
2024-12-10 08:33:42 +0530

    devops: deployment fixes about featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,11 +6,11 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, It's me,Krish!</b> Welcome to my tech blogs!!
+        <b>Hey, It&apos;s me, Krish!</b> Welcome to my tech blogs!!
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
-          <Image src="/p1.jpeg" alt="" fill className={styles.image} fill />
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
         </div>
         <div className={styles.textContainer}>
           <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet alim consectetur adipisicing elit.</h1>
